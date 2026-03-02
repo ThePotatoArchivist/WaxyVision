fd65442405a3205991661d9dece4886a7782ca2a
ThePotatoArchivist <josiahfu@gmail.com>
2026-03-02 00:13:05 -0800

    Maybe fix race condition?

--- src/main/java/archives/tater/waxyvision/OverlayModels.java
+++ src/main/java/archives/tater/waxyvision/OverlayModels.java
@@ -5,7 +5,9 @@ import com.mojang.serialization.JsonOps;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
-import net.minecraft.server.packs.resources.PreparableReloadListener;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
+import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -17,44 +19,48 @@ import org.jspecify.annotations.Nullable;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class OverlayModels implements PreparableReloadListener {
+public class OverlayModels extends SimplePreparableReloadListener<List<OverlayModels.UnbakedEntry>> {
     public static final String PATH = "waxyvision/overlays";
     private final List<Entry> entries = new ArrayList<>();
     private final Map<Block, Entry> overlays = new IdentityHashMap<>();
 
     @Override
-    public CompletableFuture<Void> reload(SharedState sharedState, Executor exectutor, PreparationBarrier barrier, Executor applyExectutor) {
-        overlays.clear();
-        entries.clear();
-        return CompletableFuture.runAsync(() -> sharedState.resourceManager().listResources(PATH, id -> true).entrySet()
+    protected List<UnbakedEntry> prepare(ResourceManager resourceManager, ProfilerFiller profiler) {
+        return resourceManager.listResources(PATH, id -> true).entrySet()
                 .stream().flatMap(entry -> {
                     var resource = entry.getValue();
                     try (var reader = resource.openAsReader(); var jsonReader = new JsonReader(reader)) {
-                        return UnbakedEntry.CODEC.parse(JsonOps.INSTANCE, JsonParser.parseReader(jsonReader))
-                                .resultOrPartial()
-                                .stream();
+                        var result = UnbakedEntry.CODEC.parse(JsonOps.INSTANCE, JsonParser.parseReader(jsonReader));
+                        result.ifError(message -> {
+                            WaxyVision.LOGGER.error("Failed to read {}: {}", entry.getKey(), message);
+                        });
+                        return result.resultOrPartial().stream();
                     } catch (IOException e) {
                         WaxyVision.LOGGER.error("Failed to read", e);
                         return Stream.of();
                     }
                 })
-                .forEach(unbakedEntry -> {
-                    var builder = new StateDefinition.Builder<Block, BlockState>(Blocks.AIR);
-                    var properties = unbakedEntry.blocks.stream().flatMap(block -> block.getStateDefinition().getProperties().stream()).collect(Collectors.toSet());
-                    for (var property : properties)
-                        builder.add(property);
-                    var definition = builder.create(Block::defaultBlockState, BlockState::new);
-                    var entry = new Entry(unbakedEntry.model, definition);
-                    entries.add(entry);
-                    for (var block : unbakedEntry.blocks)
-                        overlays.put(block, entry);
-                })
-        ).thenCompose(barrier::wait); // TODO fix race condition
+                .toList();
+    }
+
+    @Override
+    protected void apply(List<UnbakedEntry> object, ResourceManager resourceManager, ProfilerFiller profiler) {
+        overlays.clear();
+        entries.clear();
+        object.forEach(unbakedEntry -> {
+            var builder = new StateDefinition.Builder<Block, BlockState>(Blocks.AIR);
+            var properties = unbakedEntry.blocks.stream().flatMap(block -> block.getStateDefinition().getProperties().stream()).collect(Collectors.toSet());
+            for (var property : properties)
+                builder.add(property);
+            var definition = builder.create(Block::defaultBlockState, BlockState::new);
+            var entry = new Entry(unbakedEntry.model, definition);
+            entries.add(entry);
+            for (var block : unbakedEntry.blocks)
+                overlays.put(block, entry);
+        });
     }
 
     public @Nullable Entry getEntry(Block block) {
--- src/main/java/archives/tater/waxyvision/mixin/ModelManagerMixin.java
+++ src/main/java/archives/tater/waxyvision/mixin/ModelManagerMixin.java
@@ -2,26 +2,34 @@ package archives.tater.waxyvision.mixin;
 
 import archives.tater.waxyvision.WaxyVision;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 import net.minecraft.client.resources.model.BlockStateModelLoader;
 import net.minecraft.client.resources.model.ModelManager;
+import net.minecraft.server.packs.resources.PreparableReloadListener;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.Unit;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @Mixin(value = ModelManager.class, priority = 1500)
 public class ModelManagerMixin {
 
-    @ModifyExpressionValue(
+    @WrapOperation(
             method = "reload",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/model/BlockStateModelLoader;loadBlockStates(Lnet/minecraft/server/packs/resources/ResourceManager;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;")
     )
-    private CompletableFuture<BlockStateModelLoader.LoadedModels> saveModels(CompletableFuture<BlockStateModelLoader.LoadedModels> original) {
-        return original.thenApply(models -> {
-            WaxyVision.loadedModels = models;
-            return models;
-        });
+    private CompletableFuture<BlockStateModelLoader.LoadedModels> saveModels(ResourceManager resourceManager, Executor executor, Operation<CompletableFuture<BlockStateModelLoader.LoadedModels>> original, @Local(argsOnly = true) PreparableReloadListener.PreparationBarrier preparationBarrier) {
+        return preparationBarrier.wait(Unit.INSTANCE)
+                .thenComposeAsync(ignored -> original.call(resourceManager, executor))
+                .thenApply(models -> {
+                    WaxyVision.loadedModels = models;
+                    return models;
+                });
     }
 }
